23e1ac3c9d9f5f8aa7b95d058ac2e7d253da2f13
Ryan <ryan.jj.gordon@gmail.com>
2022-09-08 11:36:45 -0400

    implemented state and FSM variables

--- FSM.py
+++ FSM.py
@@ -2,3 +2,7 @@ from State import State
 
 class FSM:
     states = []
+    alphabet = []
+    delta = [[],[],[]]  # [[from state],[transition symbol],[to state]]
+    initial: State = None
+    accepting = []
--- State.py
+++ State.py
@@ -1,2 +1,4 @@
 class State:
-    
+    index: int = 0
+    accepting = False
+    initial = False
