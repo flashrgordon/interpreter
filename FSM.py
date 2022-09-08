2eddafd5b7016d153182b9ba9a9904ffb174973d
Ryan <ryan.jj.gordon@gmail.com>
2022-09-08 12:05:20 -0400

    partially implemented state and FSM transitions

--- FSM.py
+++ FSM.py
@@ -1,21 +1,17 @@
 from State import State
 
 class FSM:
-    states = []
-    alphabet = []
-    delta = [[],[],[]]  # [[from state],[transition symbol],[to state]]
-    initial: State = None
-    accepting = []
+    states = [] # list of state objects (not indices)
+    alphabet = []   # list of alphabet strings (sigma)
+    delta = [[],[],[]]  # [[from state (index)],[transition symbol (str)],[to state (index)]]
+    initial: State = None    # single initial state
+    accepting = []   # list of accepting states
+
 
     ### Constructors ###
     # default
-    def __init__(self):   # default FSM accepts only the empty string
-        q0 = State()  # q1 is the initial and accepting state with no transitions
-        self.states = [q0]  # q1 is the only state
-        self.alphabet = []
-        self.delta = [[], [], []]
-        self.initial = q0
-        self.accepting = [q0]
+    def __init__(self):   # default FSM accepts no strings
+        print('Empty FSM instantiated.')
 
     # parameterized
     def __init__(self, Q, sigma, delta, q0, q_acc):
@@ -24,4 +20,24 @@ class FSM:
         self.delta = delta
         self.initial = q0
         self.accepting = q_acc
-        
+
+
+    def addState(self, accepting, initial, transitions):    # transitions should be [[transition symbol (str)], [to state (index)]]
+        state = State(len(self.states), accepting, initial, transitions)
+
+    def addTransitions(self):   # transitions must be added after all states in Q to avoid transitions to nonexistent states
+        for i in self.states:
+            transitions = i.getTransitions()
+            if (len(transitions[0]) > 0):
+                for j in range(len(transitions[0])):
+                    self.delta[0].append(i.getIndex())   # from state index
+                    self.delta[1].append(transitions[0][j])     # transition symbol
+                    self.delta[2].append(transitions[1][j])      # to state index
+
+
+
+    def getStateByIndex(self, i):
+        if (i > 0):
+            return self.states[i-1]
+        else:
+            return None
--- State.py
+++ State.py
@@ -2,6 +2,7 @@ class State:
     index: int = 0
     accepting = False
     initial = False
+    transitions = [[], []]  # [[transition symbol (str)], [to state (index)]]
 
     ### Constructors ###
     # default
@@ -9,9 +10,20 @@ class State:
         self.index = 1
         self.accepting = True
         self.initial = True
+        transitions = [[], []]
 
     # parameterized
-    def __init__(self, index, accepting, initial):
+    def __init__(self, index, accepting, initial, transitions):
         self.index = index
         self.accepting = accepting
         self.initial = initial
+        self.transitions = transitions
+
+
+
+
+    def getIndex(self):
+        return self.index
+
+    def getTransitions(self):
+        return self.transitions
